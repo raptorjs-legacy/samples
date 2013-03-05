714fc9d1ff122641a6155f163f93ef3172795cdc
Patrick Steele-Idem <psteeleidem@ebay.com>
2013-03-04 19:03:53 -0700

    Fixed server script for the tutorial app

--- tutorial/server.js
+++ tutorial/server.js
@@ -4,7 +4,7 @@ var app = express();
 //app.get('/', function(req, res){
 //    res.send('Hello World');
 //});
-app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/build'));
 
 
 app.listen(8090);
